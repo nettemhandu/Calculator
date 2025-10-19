86f8e9114c62127c41866b1ed0ce919d9e580bb5
nettemhandu <antonetteliebe@icloud.com>
2025-10-19 09:34:09 +0200

    added subtraction test

--- src/src/test/java/CalculatorTest.java
+++ src/src/test/java/CalculatorTest.java
@@ -15,5 +15,12 @@ public class CalculatorTest {
         assertEquals(5, result, "2 + 3 should equal 5");
     }
 
+    @Test
+    void testSubtraction() {
+        Calculator calculator = new Calculator();
+        int result = calculator = calculator.subtract(5, 3);
+        assertEquals(2, result, "5 - 3 should equal 2");
+    }
+
 
 }
