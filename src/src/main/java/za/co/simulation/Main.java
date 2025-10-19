7b36dcee08bfb0300252dde21d800beb08fa32a3
nettemhandu <antonetteliebe@icloud.com>
2025-10-19 15:21:32 +0200

    tests are all passing

--- src/src/main/java/za/co/simulation/Main.java
+++ src/src/main/java/za/co/simulation/Main.java
@@ -3,16 +3,17 @@ package za.co.simulation;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int result = calculator.add(1, 1);
-        System.out.println("Answer is: " + result);
+
+        int result = calculator.add(5, 5);
+        System.out.println("Answer for addition is: " + result);
 
         int result1 = calculator.subtract(5,2);
-        System.out.println("Answer is: " + result);
+        System.out.println("Answer for subtract is: " + result1);
 
-        int result1 = calculator.divide(5,2);
-        System.out.println("Answer is: " + result);
+        int result2 = calculator.divide(5,2);
+        System.out.println("Answer for division is: " + result2);
 
-        int result1 = calculator.multiply(5,2);
-        System.out.println("Answer is: " + result);
+        int result3 = calculator.multiply(5,2);
+        System.out.println("Answer for multiplication is: " + result3);
     }
 }
