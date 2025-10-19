4cf83195cac0c57c8346774418c65bb1dfb8d2ed
nettemhandu <antonetteliebe@icloud.com>
2025-10-19 09:46:51 +0200

    initialized add method

--- src/src/main/java/za/co/simulation/Main.java
+++ src/src/main/java/za/co/simulation/Main.java
@@ -3,10 +3,9 @@ package za.co.simulation;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        int result = calculator.add(1, 1);
+        System.out.println("Answer is: " + result);
 
-    }
-
-    public static add() {
 
     }
 }
