25abc459aa6eadb328bfefd9f8b8058dd066a6cf
nettemhandu <antonetteliebe@icloud.com>
2025-10-21 14:01:57 +0200

    added comments to Calculator class

--- src/src/main/java/za/co/simulation/Calculator.java
+++ src/src/main/java/za/co/simulation/Calculator.java
@@ -2,18 +2,22 @@ package za.co.simulation;
 
 public class Calculator {
 
+    // An addition method
     public int add(int x, int y) {
         return x + y;
     }
 
+    // A subtraction method
     public int subtract(int x, int y) {
         return x - y;
     }
 
+    // A divide method
     public int divide(int x, int y) {
         return x / y;
     }
 
+    // A multiplication method
     public int multiply(int x, int y) {
         return x * y;
     }
